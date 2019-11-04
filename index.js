9aad286f249a1adb39786cf2f709634072532be9
mark <mark@hexlant.com>
2019-11-04 13:25:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var litecore = module.exports;
 
 // module information
 litecore.version = 'v' + require('./package.json').version;
-litecore.versionGuard = function(version) {
+litecore.versionGuard = function(version) { return;
   if (version !== undefined) {
     var message = 'More than one instance of litecore-lib found. ' +
       'Please make sure to require litecore-lib and check that submodules do' +
